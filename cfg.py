66ae2fb1d852b3e0c4b980be5c5d5b809ccdc70e
slowriding <48739803+slowriding@users.noreply.github.com>
2021-01-22 12:01:15 -0500

    Master

--- cfg.py
+++ cfg.py
@@ -2,3 +2,11 @@
 runcommand="./Env/prod/OSX/main.app/Contents/MacOS/Game"
 #relative to OS Linux,OSX,Win64 are supported
 OS="OSX"
+
+if OS=="OSX":
+    Pathtoexe=f"Env/prod/{OS}/main.app/Contents/MacOS/Game"
+elif OS=="Win64":
+    Pathtoexe=f"Env/prod/{OS}/Game.exe"
+elif OS=="Linux":
+    Pathtoexe=f"Env/prod/{OS}/Game"
+
